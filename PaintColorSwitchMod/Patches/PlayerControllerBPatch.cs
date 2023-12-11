a8ca85d066dd56b4a837c4619c977bf1612b1156
Silvision <richardmnguyen@hotmail.com>
2023-12-11 17:05:45 -0600

    initial commit

---
+++ PaintColorSwitchMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GameNetcodeStuff;
+using HarmonyLib;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace PaintColorSwitchMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    public class PlayerControllerBPatch
+    {
+        
+        private static GrabbableObject currentItem;
+        private static SprayPaintItem sprayPaintItem = null;
+        
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void UpdatePaintColorPatch(PlayerControllerB __instance)
+        {
+            if ((!(__instance).IsOwner || !__instance.isPlayerControlled || ((__instance).IsServer && !__instance.isHostPlayerObject)) && !__instance.isTestingPlayer)
+            {
+                currentItem = __instance.ItemSlots[__instance.currentItemSlot];
+                sprayPaintItem = currentItem as SprayPaintItem;
+                if (sprayPaintItem != null)
+                {
+                    Debug.Log(sprayPaintItem.GetType());
+                }
+                
+            }
+        }
+    }
+}
---
+++ PaintColorSwitchMod/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using BepInEx;
+using HarmonyLib;
+using PaintColorSwitchMod.Patches;
+
+namespace PaintColorSwitchMod
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class PaintColorSwitchModBase : BaseUnityPlugin
+    {
+        private const string PLUGIN_GUID = "Silvision.PaintColorSwitchMod";
+        private const string PLUGIN_NAME = "Paint Color Switch Mod";
+        private const string PLUGIN_VERSION = "1.0.0";
+        
+        private readonly Harmony harmony = new Harmony(PLUGIN_GUID);
+
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+            harmony.PatchAll(typeof(PaintColorSwitchModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
